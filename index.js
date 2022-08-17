6ed7f642f0f6d7b89b7be355afd04da7bcaae2fe
sorcerer.worker <sorcerer.worker@yandex.com>
2022-08-17 05:17:27 -0400

    feat: Add once ready messages

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const env = require("dotenv");
 
 env.config()
 
-console.log(process.env.TOKEN)
-
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages
 ] });
+
+client.once("ready", (bot) => {
+    console.log("Status: Successfully ran!")
+    console.log(`Running on: ${bot.user.username}`)
+    console.log(`Using Token: ${process.env.TOKEN}`)
+})
 
 
 client.login(process.env.TOKEN);
